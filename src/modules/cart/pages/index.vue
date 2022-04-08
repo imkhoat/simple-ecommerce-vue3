46b781ec1bcd1049e547c2ffd27392e3cfe354b5
imkhoat <khoatbui.idea@gmail.com>
2022-04-08 13:16:37 +0700

    feat: implement cart page

--- src/modules/cart/pages/index.vue
+++ src/modules/cart/pages/index.vue
@@ -1,17 +1,69 @@
 <template>
-    <div class="page-category">
-        <div class="categories">
-            <s-category-card class="item" v-for="i in 5"></s-category-card>
+    <div class="page-cart">
+        <div class="card">
+            <div class="items">
+                <product-cart-item
+                    :item="product"
+                    class="item"
+                    v-for="(product, index) in products"
+                    :key="index + 'page-cart-item'"
+                ></product-cart-item>
+            </div>
+            <div class="amount">{{ totalAmountByText }}</div>
         </div>
     </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive, ref, computed } from 'vue'
+import { useCartStore } from '../../../shared/stores/cart'
 
-import SCategoryCard from '../../../shared/components/molecules/s-category-card/s-category-card.vue'
+import ProductCartItem from '../components/product-cart-item.vue'
 export default defineComponent({
     components: {
-        SCategoryCard,
+        ProductCartItem,
+    },
+    setup() {
+        const cart = useCartStore()
+
+        const products = cart.pickupItems
+        const totalAmount = cart.totalAmount
+
+        const totalAmountByText = computed(() => {
+            return (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'USD' }).format(totalAmount))
+        })
+
+        return { products, totalAmount, totalAmountByText }
     }
 })
 </script>
+<style>
+.page-cart {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 24px;
+    background-color: rgb(249, 250, 251);
+}
+.page-cart .card {
+    width: 550px;
+    border-radius: 8px;
+    background-color: #fff;
+    box-shadow: rgba(0, 0, 0, 0) 0px 0px 0px 0px,
+        rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0.1) 0px 1px 3px 0px,
+        rgba(0, 0, 0, 0.1) 0px 1px 2px -1px;
+}
+.page-cart .items {
+    display: flex;
+    flex-direction: column;
+}
+.page-cart .items .item {
+    border-bottom: 1px solid rgb(229, 231, 235);
+}
+.page-cart .card .amount {
+    text-align: right;
+    padding: 24px;
+    font-size: 24px;
+    font-weight: bold;
+}
+</style>
--- src/shared/stores/cart.ts
+++ src/shared/stores/cart.ts
@@ -5,10 +5,52 @@ export const useCartStore = defineStore("cart", {
   state: () => {
     return { cart: [] as CartItem[] };
   },
+  getters: {
+    pickupItems: (state) => state.cart,
+    totalAmount: (state) => {
+      return state.cart.reduce((previousValue, currentValue) => {
+        const quantity = currentValue?.quantity as number;
+        const price = currentValue?.product.price as number;
+        const total = quantity * price;
+        return previousValue + total;
+      }, 0);
+    },
+  },
 
   actions: {
     addToCart(payload: CartItem) {
-      this.cart.push(payload);
+      const targetIndex = this.cart.findIndex(
+        (item) => item.product.code === payload.product.code
+      );
+      if (targetIndex != -1) {
+        const currentQuantity = this.cart[targetIndex].quantity as number;
+        const topupQuantity = payload.quantity as number;
+        this.cart[targetIndex].quantity = currentQuantity + topupQuantity;
+      } else {
+        this.cart.push(payload);
+      }
+    },
+    increaseQuantity(payload: CartItem) {
+      const targetIndex = this.cart.findIndex(
+        (item) => item.product.code === payload.product.code
+      );
+      if (targetIndex != -1) {
+        const currentQuantity = this.cart[targetIndex].quantity as number;
+        this.cart[targetIndex].quantity = currentQuantity + 1;
+      } else {
+        this.cart.push(payload);
+      }
+    },
+    decreaseQuantity(payload: CartItem) {
+      const targetIndex = this.cart.findIndex(
+        (item) => item.product.code === payload.product.code
+      );
+      if (targetIndex != -1) {
+        const currentQuantity = this.cart[targetIndex].quantity as number;
+        this.cart[targetIndex].quantity = currentQuantity - 1;
+      } else {
+        this.cart.push(payload);
+      }
     },
   },
 });
